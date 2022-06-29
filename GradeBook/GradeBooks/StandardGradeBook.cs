75a2604d0d60f299af9eda6ee16b587a12bacf48
Shane Kaestner <shane.kaestner@gmail.com>
2022-06-29 15:56:46 -0700

    Check My Work

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        private static string name;
+
+        public StandardGradeBook() : base(name)
+        {
+        }
+    }
+}
