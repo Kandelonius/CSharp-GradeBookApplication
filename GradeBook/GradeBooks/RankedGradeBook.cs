a74436c7ea401e9b92a15b8bb00e109bb4494c98
Shane Kaestner <shane.kaestner@gmail.com>
2022-06-29 17:25:09 -0700

    Check My Work

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -43,7 +43,8 @@ namespace GradeBook.GradeBooks
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
-            base.CalculateStatistics();
+            else
+                base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
@@ -52,7 +53,8 @@ namespace GradeBook.GradeBooks
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
-            base.CalculateStudentStatistics(name);
+            else
+                base.CalculateStudentStatistics(name);
         }
     }
 }
